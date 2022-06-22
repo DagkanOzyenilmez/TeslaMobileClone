36665996d71d05cdf870007108e7d88d53650ce3
DagkanOzyenilmez <ozyenilmez.dagkan@gmail.com>
2022-06-22 19:42:05 +0300

    First Commit

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet,View} from 'react-native';
+
+import CarItem from './components/CarItem';
+
+function App() {
+  return (
+    
+    <View style={styles.container}>
+      <CarItem />
+    </View>  
+
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+});
+
+
+
+
+
+export default App;
